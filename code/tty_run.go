8454fab30374466bf386e2b710afb47d2dd23dc9
Ryotaro <ryotaro612.ooo@gmail.com>
2024-11-29 09:50:23 +0900

    re

--- code/tty_run.go
+++ code/tty_run.go
@@ -1,8 +1,6 @@
-func (t *tty) recvtty(socket *os.File) (Err error) {
-  // 中略
-  f, err := utils.RecvFile(socket)
-  go func() {_, _ = io.Copy(epollConsole, os.Stdin)}()
-  t.wg.Add(1)
-  go t.copyIO(os.Stdout, epollConsole)
-  // 中略
-}
+f, err := utils.RecvFile(socket)
+go func() {
+  _, _ = io.Copy(epollConsole, os.Stdin)
+}()
+t.wg.Add(1)
+go t.copyIO(os.Stdout, epollConsole)
