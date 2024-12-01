3b2ce743720d9b1fa1ab0a9d016c7ea53414c0c0
Ryotaro <ryotaro612.ooo@gmail.com>
2024-12-02 03:41:35 +0900

    revise

--- code/run.sh
+++ code/run.sh
@@ -1,6 +1,7 @@
 mkdir rootfs
 # busyboxの/以下をrootfsにコピー
-docker export $(docker create busybox) | tar -C rootfs -xvf -
+docker export $(docker create busybox) | \
+tar -C rootfs -xvf -
 # コンテナを起動するパラメタの雛形を./config.jsonに出力
 runc spec
 # コンテナIDをcidとしてコンテナを起動
