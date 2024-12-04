16d64054f64dc7a85e7cb45e3b1c4171b46ea725
Ryotaro <ryotaro612.ooo@gmail.com>
2024-12-05 02:23:56 +0900

    a

--- code/pivot_root.sh
+++ code/pivot_root.sh
@@ -1,6 +1,7 @@
 cd rootfs && mkdir put_old
 # 新しいマウントポイントの名前空間に移動
 unshare -mpfr /bin/sh
+# pivot_rootの引数はマウントポイントでないとだめ
 mount --bind $(pwd) $(pwd)
 # put_oldにもとのルートをおく。後でunmount可
 pivot_root $(pwd) $(pwd)/put_old
